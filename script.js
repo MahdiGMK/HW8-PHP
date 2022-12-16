b653d648ae7ba00252b07cfec136bd425f0adf25
MahdiGmk <mbahramiand@gmail.com>
2022-12-16 21:56:13 +0330

    test bugfix?

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function Submit() {
 	if (!confirm('آیا از فرستادن پیام مطمئن هستید؟')) {
 	}
-	else 
-	{
+	else {
 		const xhttp = new XMLHttpRequest();
 		xhttp.onload = function () {
 			// alert(this.statusText);
@@ -12,11 +11,12 @@ function Submit() {
 		let mail = document.getElementById("mail").value;
 		let msg = document.getElementById("msg").value;
 
-		xhttp.open("POST" , "backend.php");
-		xhttp.setRequestHeader("Name" , name);
-		xhttp.setRequestHeader("Mail" , mail);
-		xhttp.setRequestHeader("Msg" , msg);
-		xhttp.send();
+		xhttp.open("POST", "backend.php");
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		// xhttp.setRequestHeader("Name", name);
+		// xhttp.setRequestHeader("Mail", mail);
+		// xhttp.setRequestHeader("Msg", msg);
+		xhttp.send("Name=" + name + "  Mail="+ mail + "  Msg=" + msg);
 	}
 	return false;
 }
