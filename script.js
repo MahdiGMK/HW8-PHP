4136ac13f1c89299b1c71559375e03dc3b917849
MahdiGmk <mbahramiand@gmail.com>
2022-12-16 22:11:18 +0330

    bug fix 2?

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 function Submit() {
 	if (!confirm('آیا از فرستادن پیام مطمئن هستید؟')) {
+		return false;
 	}
 	else {
-		const xhttp = new XMLHttpRequest();
-		xhttp.onload = function () {
-			// alert(this.statusText);
-			alert('پیام شما با موفقیت ارسال شد.');
-		}
-		let name = document.getElementById("name").value;
-		let mail = document.getElementById("mail").value;
-		let msg = document.getElementById("msg").value;
-
-		xhttp.open("POST", "backend.php");
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		// xhttp.setRequestHeader("Name", name);
-		// xhttp.setRequestHeader("Mail", mail);
-		// xhttp.setRequestHeader("Msg", msg);
-		xhttp.send("Name=" + name + "  Mail="+ mail + "  Msg=" + msg);
+		return true;
 	}
-	return false;
+	// talash kardam haminja vasl sham be php nashod
+	// mitonid talasham ro ba estefade az git bebinid
 }
