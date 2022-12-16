65aacf6bee311d632317d9ba9e3495282eb9f09e
MahdiGmk <mbahramiand@gmail.com>
2022-12-16 10:31:10 +0330

    adding backend

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function Submit(){
-	if(!confirm('آیا از فرستادن پیام مطمئن هستید؟')){
+function Submit() {
+	if (!confirm('آیا از فرستادن پیام مطمئن هستید؟')) {
 		return false;
 	}
-	else{
-		alert('پیام شما با موفقیت ارسال شد.');
+	else {
+		const xhttp = new XMLHttpRequest();
+		xhttp.onload = function () {
+			// alert(this.statusText);
+			alert('پیام شما با موفقیت ارسال شد.');
+		}
+
+		xhttp.open("POST" , "backend.php");
+		xhttp.setRequestHeader("Name" , "Mahdi");
+		xhttp.send();
+
 		return true;
 	}
 }
