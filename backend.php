4834e953101e1bfb58498e084d532523f45c3c0e
MahdiGmk <mbahramiand@gmail.com>
2022-12-16 21:25:44 +0330

    complete backend

--- backend.php
+++ backend.php
@@ -1,1 +1,15 @@
-$name = _REQUEST["Name"]
+<?php
+$name = $_POST["ٔName"];
+$mail = $_POST["Mail"];
+$massage = $_POST["Msg"];
+
+$cnt = count (glob ('*.txt'));
+$fname = ( $cnt + 1) . '.txt';
+
+$file = fopen("$fname","w");
+echo fwrite($file,"NAME: " . $name . " \r\n");
+echo fwrite($file,"EMAIL: " . $mail . " \r\n");
+echo fwrite($file,"MASSAGE: " . $massage . " \r\n");
+fclose($file);
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function Submit() {
 	if (!confirm('آیا از فرستادن پیام مطمئن هستید؟')) {
-		return false;
 	}
-	else {
+	else 
+	{
 		const xhttp = new XMLHttpRequest();
 		xhttp.onload = function () {
 			// alert(this.statusText);
 			alert('پیام شما با موفقیت ارسال شد.');
 		}
+		let name = document.getElementById("name").value;
+		let mail = document.getElementById("mail").value;
+		let msg = document.getElementById("msg").value;
 
 		xhttp.open("POST" , "backend.php");
-		xhttp.setRequestHeader("Name" , "Mahdi");
+		xhttp.setRequestHeader("Name" , name);
+		xhttp.setRequestHeader("Mail" , mail);
+		xhttp.setRequestHeader("Msg" , msg);
 		xhttp.send();
-
-		return true;
 	}
+	return false;
 }
